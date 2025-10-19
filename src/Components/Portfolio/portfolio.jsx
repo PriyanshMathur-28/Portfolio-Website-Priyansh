8eab4617c3f3d364904f6f7c192410db55db60ba
Priyansh Mathur <priyanshmathur.aisc@gmail.com>
2025-10-20 01:58:16 +0530

    Update portfolio.jsx

--- src/Components/Portfolio/portfolio.jsx
+++ src/Components/Portfolio/portfolio.jsx
@@ -1,14 +1,23 @@
-import "./portfolio.css"
+import "./portfolio.css";
 import Work from "./work";
 
 const Portfolio = () => {
   return (
     <section className="work section" id="portfolio">
-        {/* <h2 className="section_title">Portfolio</h2> */}
-        <span className="section_subtitle">Portfolio Coming Soon...</span>
-        <Work/>
-    </section> 
-)
+      <span className="section_subtitle">
+        Projects on GitHub ➡️{" "}
+        <a 
+          href="https://github.com/PriyanshMathur-28" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="github-link"
+        >
+          GitHub
+        </a>
+      </span>
+      <Work />
+    </section>
+  );
 }
 
 export default Portfolio;
